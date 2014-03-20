df9ec7decd52623c7f95621aa34120984d622d75
Andy Goldstein <andy.goldstein@gmail.com>
2014-03-20 09:40:33 -0400

    Prep for MBean sources
    Update MetricJob to get ready to support MBean based sources.

--- src/main/java/com/openshift/metrics/extension/MetricJob.java
+++ src/main/java/com/openshift/metrics/extension/MetricJob.java
@@ -21,122 +21,129 @@ import org.quartz.JobExecutionException;
 import org.quartz.SchedulerException;
 
 public class MetricJob implements Job {
-	private final Logger log = Logger.getLogger(MetricJob.class);
-	
-	private ModelControllerClient getModelControllerClient(JobExecutionContext context) {
-		ModelControllerClient client = null;
-		
-		try {
-			client = (ModelControllerClient) context.getScheduler().getContext().get(MODEL_CONTROLLER_CLIENT);
-		} catch (SchedulerException e) {
-			log.error("Unable to retrieve model controller client from job/scheduler context", e);
-			//TODO do we need to consider unscheduling this job if we fail to get the client
-			//some # of times?
-		}
-		
-		return client;
-	}
-	
-	/**
-	 * Set the {@code operation}'s address from the {@code source}.
-	 * 
-	 * @param operation the operation
-	 * @param source a String with a format such as subsystem=foo/type=bar/other=baz
-	 */
-	private void setAddressFromSource(ModelNode operation, String source) {
-		ModelNode address = operation.get(OP_ADDR);
-		
-		StringTokenizer tokenizer = new StringTokenizer(source, "/");
+    private final Logger log = Logger.getLogger(MetricJob.class);
 
-		// split on /
-		while(tokenizer.hasMoreTokens()) {
-			// each token will be of the form left=right
-			// split into an array
-			String[] kv = tokenizer.nextToken().split("=");
-			
-			address.add(kv[0], kv[1]);
-		}
-	}
-	
-	public void execute(JobExecutionContext context) throws JobExecutionException {
-		ModelControllerClient modelControllerClient = getModelControllerClient(context);
-		
-		if(null == modelControllerClient) {
-			// without a client, we can't do much
-			return;
-		}
-		
-		// the job data map has the metric sources associated with this job
-		final JobDataMap jobDataMap = context.getMergedJobDataMap();
+    private ModelControllerClient getModelControllerClient(JobExecutionContext context) {
+        ModelControllerClient client = null;
 
-		// grab the sources
-		Map<String, Map<String, String>> sources = (Map<String, Map<String, String>>) jobDataMap.get(METRIC_SOURCES);
-		
-		// loop through each source, processing all of each source's metrics
-		for (String source : sources.keySet()) {
-			// create an operation request
-			ModelNode op = new ModelNode();
-			
-			// set the operation to read-attribute
-			op.get(OP).set(READ_ATTRIBUTE_OPERATION);
-			
-			// set the address appropriately
-			setAddressFromSource(op, source);
-			
-			// grab this source's metrics
-			Map<String, String> metrics = sources.get(source);
-			
-			// loop through them
-			for(String originalKey : metrics.keySet()) {
-				// originalKey could be something like profile-name or it could be
-				// an index into a map, such as usage.bytes
-				String key = originalKey;
-				
-				// subkey will represent the index into a map, if it exists
-				String subkey = null;
-				
-				//TODO consider supporting indexing into arrays
-//				final int leftBracketIndex = key.indexOf('[');
-				
-				final int dotIndex = key.indexOf('.');
-				if(dotIndex > -1) {
-					// if we found a . then there's a subkey (e.g. bytes from usage.bytes)
-					subkey = key.substring(dotIndex + 1);
-					
-					// update key to be the actual attribute name (e.g. usage from usage.bytes)
-					key = key.substring(0, dotIndex);
-				}
-				
-				// set the name of the attribute we want
-				op.get(ModelDescriptionConstants.NAME).set(key);
-				
-				try {
-					// execute the request
-					ModelNode r = modelControllerClient.execute(op);
-					
-					// TODO consider checking the status
-					// get the result
-					ModelNode result = r.get(ModelDescriptionConstants.RESULT);
-					
-					if(subkey != null) {
-						// if we had a subkey, set the result to its value
-						result = result.get(subkey);
-					}
-					
-					String publishName = metrics.get(originalKey);
-					String metricValue = result.asString();
-					publishMetric(publishName, metricValue);
-				} catch (IOException e) {
-					log.error("Error executing operation " + op, e);
-					// TODO anything else to do here?
-				}
-			}
-			
-		}
-	}
+        try {
+            client = (ModelControllerClient) context.getScheduler().getContext().get(MODEL_CONTROLLER_CLIENT);
+        } catch (SchedulerException e) {
+            log.error("Unable to retrieve model controller client from job/scheduler context", e);
+            //TODO do we need to consider unscheduling this job if we fail to get the client
+            //some # of times?
+        }
 
-	private void publishMetric(String publishName, String metricValue) {
-		//TODO switch to syslog?
-		log.info(publishName + "=" + metricValue);
-	}
+        return client;
+    }
+
+    /**
+     * Set the {@code operation}'s address from the {@code source}.
+     *
+     * @param operation the operation
+     * @param path a String with a format such as subsystem=foo/type=bar/other=baz
+     */
+    private void setAddressFromSource(ModelNode operation, String path) {
+        ModelNode address = operation.get(OP_ADDR);
+
+        StringTokenizer tokenizer = new StringTokenizer(path, "/");
+
+        // split on /
+        while(tokenizer.hasMoreTokens()) {
+            // each token will be of the form left=right
+            // split into an array
+            String[] kv = tokenizer.nextToken().split("=");
+
+            address.add(kv[0], kv[1]);
+        }
+    }
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        // the job data map has the metric sources associated with this job
+        final JobDataMap jobDataMap = context.getMergedJobDataMap();
+
+        // grab the sources
+        Map<Source, Map<String, String>> sources = (Map<Source, Map<String, String>>) jobDataMap.get(METRIC_SOURCES);
+
+        // loop through each source, processing all of each source's metrics
+        for (Source source : sources.keySet()) {
+            if(source.ismBean()) {
+                //		        doMBeanSource(source);
+            } else {
+                //TODO consider switching jobDataMap(METRIC_SOURCES) to a List or Set
+                //and add the metrics to the Source class itself
+                doNativeSource(source, sources.get(source), context);
+            }
+        }
+    }
+
+    private void doNativeSource(Source source, Map<String, String> metrics, JobExecutionContext context) {
+        ModelControllerClient modelControllerClient = getModelControllerClient(context);
+
+        if(null == modelControllerClient) {
+            // without a client, we can't do much
+            return;
+        }
+
+        // create an operation request
+        ModelNode op = new ModelNode();
+
+        // set the operation to read-attribute
+        op.get(OP).set(READ_ATTRIBUTE_OPERATION);
+
+        // set the address appropriately
+        setAddressFromSource(op, source.getPath());
+
+        // loop through them
+        for(String originalKey : metrics.keySet()) {
+            // originalKey could be something like profile-name or it could be
+            // an index into a map, such as usage.bytes
+            String key = originalKey;
+
+            // subkey will represent the index into a map, if it exists
+            String subkey = null;
+
+            //TODO consider supporting indexing into arrays
+            //          final int leftBracketIndex = key.indexOf('[');
+
+            final int dotIndex = key.indexOf('.');
+            if(dotIndex > -1) {
+                // if we found a . then there's a subkey (e.g. bytes from usage.bytes)
+                subkey = key.substring(dotIndex + 1);
+
+                // update key to be the actual attribute name (e.g. usage from usage.bytes)
+                key = key.substring(0, dotIndex);
+            }
+
+            // set the name of the attribute we want
+            op.get(ModelDescriptionConstants.NAME).set(key);
+
+            try {
+                // execute the request
+                ModelNode r = modelControllerClient.execute(op);
+
+                // TODO consider checking the status
+                // get the result
+                ModelNode result = r.get(ModelDescriptionConstants.RESULT);
+
+                if(subkey != null) {
+                    // if we had a subkey, set the result to its value
+                    result = result.get(subkey);
+                }
+
+                String publishName = metrics.get(originalKey);
+                String metricValue = result.asString();
+                publishMetric(publishName, metricValue);
+            } catch (IOException e) {
+                log.error("Error executing operation " + op, e);
+                // TODO anything else to do here?
+            }
+        }
+    }
+
+    private void publishMetric(String publishName, String metricValue) {
+        //TODO switch to syslog?
+        log.info(publishName + "=" + metricValue);
+    }
 }
--- src/main/java/com/openshift/metrics/extension/Source.java
+++ src/main/java/com/openshift/metrics/extension/Source.java
@@ -1,37 +1,74 @@
 package com.openshift.metrics.extension;
 
 public class Source {
-	public String path;
-	public boolean mBean;
-	public Source(String p, boolean mB) {
-		path = p;
-		mBean = mB;
-	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (mBean ? 1231 : 1237);
-		result = prime * result + ((path == null) ? 0 : path.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Source other = (Source) obj;
-		if (mBean != other.mBean)
-			return false;
-		if (path == null) {
-			if (other.path != null)
-				return false;
-		} else if (!path.equals(other.path))
-			return false;
-		return true;
-	}
-	
+    private String path;
+    private boolean mBean;
+
+    public Source(String path, boolean mBean) {
+        this.path = path;
+        this.mBean = mBean;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (mBean ? 1231 : 1237);
+        result = prime * result + ((path == null) ? 0 : path.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Source other = (Source) obj;
+        if (mBean != other.mBean) {
+            return false;
+        }
+        if (path == null) {
+            if (other.path != null) {
+                return false;
+            }
+        } else if (!path.equals(other.path)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @return the path
+     */
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * @param path the path to set
+     */
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    /**
+     * @return the mBean
+     */
+    public boolean ismBean() {
+        return mBean;
+    }
+
+    /**
+     * @param mBean the mBean to set
+     */
+    public void setmBean(boolean mBean) {
+        this.mBean = mBean;
+    }
+
 }
