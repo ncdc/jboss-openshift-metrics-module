9c110ce4e844ede778c26315c7533fad61b5081f
Andy Goldstein <andy.goldstein@gmail.com>
2014-03-25 15:19:20 -0400

    Remove unused code

--- src/main/java/com/openshift/metrics/extension/OpenShiftSubsystemDefinition.java
+++ src/main/java/com/openshift/metrics/extension/OpenShiftSubsystemDefinition.java
@@ -1,22 +1,10 @@
 package com.openshift.metrics.extension;
 
-import org.jboss.as.controller.SimpleAttributeDefinition;
-import org.jboss.as.controller.SimpleAttributeDefinitionBuilder;
 import org.jboss.as.controller.SimpleResourceDefinition;
-import org.jboss.as.controller.registry.AttributeAccess;
 import org.jboss.as.controller.registry.ManagementResourceRegistration;
-import org.jboss.dmr.ModelType;
 
 public class OpenShiftSubsystemDefinition extends SimpleResourceDefinition {
     public static final OpenShiftSubsystemDefinition INSTANCE = new OpenShiftSubsystemDefinition();
-
-    protected static final SimpleAttributeDefinition METRICS_GROUP =
-            new SimpleAttributeDefinitionBuilder(Constants.METRICS_GROUP, ModelType.LIST)
-                .setAllowExpression(false)
-                .setXmlName(Constants.METRICS_GROUP)
-                .setFlags(AttributeAccess.Flag.RESTART_ALL_SERVICES)
-                .setAllowNull(false)
-                .build();
 
     private OpenShiftSubsystemDefinition() {
         super(OpenShiftSubsystemExtension.SUBSYSTEM_PATH,
